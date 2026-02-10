2f5305c8c67a398fef2cc586e60c60727b939d89
wathasin2547 <66070183@kmitl.ac.th>
2026-02-10 17:43:44 +0700

    add file

---
+++ cell02/ex00/iszeronum.py
@@ -0,0 +1,8 @@
+def main():
+    user_input = input()
+    number = int(user_input)
+    if number == 0:
+        print("This number is equal to zero.")
+    else:
+        print("This number is different from zero.")
+main()
