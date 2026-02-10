c118e787b3be99c0eab5b8c5d9f9bc9315c85bbd
wathasin2547 <66070183@kmitl.ac.th>
2026-02-10 20:29:21 +0700

    add file

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+def main():
+    password = "Python is awesome"
+    user_input = input()
+    if user_input == password:
+        print("ACCESS GRANTED")
+    else:
+        print("ACCESS DENIED")
+main()
