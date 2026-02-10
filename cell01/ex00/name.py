9e781ec0bdc70651a37cd6eea12c73d60d5b33f4
wathasin2547 <66070183@kmitl.ac.th>
2026-02-10 15:24:44 +0700

    add file

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,6 +1,6 @@
+'''name'''
 def main():
     first_name = "Wathasin"
     last_name = "Huaihongtong"
-    whole_name = first_name + " " + last_name
-    print(whole_name)
+    print(first_name, last_name)
 main()
