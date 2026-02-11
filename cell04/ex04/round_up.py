3db22dfa8397502a3f9ab4c2415829899937b707
wathasin2547 <66070183@kmitl.ac.th>
2026-02-11 19:59:44 +0700

    add file

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+def main():
+    user_input = input("Give me a number: ")
+    number = float(user_input)
+    rounded_number = math.ceil(number)
+    print(rounded_number)
+main()
